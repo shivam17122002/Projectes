2ad083c66fd349d1c162c4adfff0c39b5b640aa2
shivam17122002 <142995456+shivam17122002@users.noreply.github.com>
2023-09-12 18:43:06 +0530

    Update Voice_tran.py

--- Voice_tran.py
+++ Voice_tran.py
@@ -3,15 +3,17 @@ import speech_recognition
 import gtts
 import playsound
 
+input_lang = "hi"
+output_lang = "fr"
 recog = speech_recognition.Recognizer()
 with speech_recognition.Microphone() as source:
     print("Speak...")
     voice = recog.listen(source)
-    text = recog.recognize_google(voice, language="fr")
+    text = recog.recognize_google(voice, language=input_lang)
     print(text)
 translator = googletrans.Translator()
-translation = translator.translate(text, dest="fr")
+translation = translator.translate(text, dest=output_lang)
 print(translation.text)
-con_audio = gtts.gTTS(translation.text, lang="fr")
+con_audio = gtts.gTTS(translation.text, lang=output_lang)
 con_audio.save("translate.mp4")
 playsound.playsound("translate.mp4")
